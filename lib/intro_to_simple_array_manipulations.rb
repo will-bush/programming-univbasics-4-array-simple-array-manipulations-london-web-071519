d7952f3096ecfb0099d910c452b1bbcdbe428a12
Will Bush <williamdcbush@gmail.com>
2019-06-26 14:46:23 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -22,8 +22,8 @@ def shift_with_args(array)
   array.shift(2)
 end
 
-#def using_concat(array,array)
-  #array.concat array
+#def using_concat(array,array2)
+  #array.concat array2
 #end
 
 def using_insert(array,element)
